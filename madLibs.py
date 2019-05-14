d32ed98e839a087ae5f8f32dd4ba1d5e9f65998d
gsg62 <38050071+gsg62@users.noreply.github.com>
2019-05-14 11:49:56 -0700

    madLibs.py
    Basic madLib from very first computer science lab.

---
+++ madLibs.py
@@ -0,0 +1,33 @@
+# Greg Geary
+# super basic first program using python
+# madd libs game
+
+
+# Ask user to enter different verbs, adjectives, adverbs and nouns using
+# the input command
+first_name = input("Enter a male name: ")
+first_adj = input("Enter an adjective: ")
+first_verb = input("Enter a verb: ")
+second_name = input("Enter another name: ")
+first_pverb = input("Enter a past tense verb: ")
+second_adj = input("Enter an adjective: ")
+second_verb = input("Enter a verb: ")
+first_adverb = input("Enter an adverb: ")
+first_noun = input("Enter a noun(thing): ")
+second_noun = input("Enter a place: ")
+third_verb = input("Enter a past tense verb: ")
+third_adj = input("Enter an adjective: ")
+
+print("My First Mad Lib Story")
+print("======================")
+print("There was a kid named "+first_name+".")
+print("He was a "+first_adj+" kind of kid.")
+print("One day, "+first_name+ " was "+first_verb+" up to "+second_name+".")
+print(second_name+" was "+first_pverb+" to see "+first_name+".")
+print(second_name+" thought "+first_name+" was a "+second_adj+" "+first_noun+".")
+print(first_name+" was shocked by that.")
+print(second_name+" "+second_verb+" away.")
+print(first_name+" "+first_adverb+" "+third_verb+" to "+second_noun+".")
+print(first_name+" now thought that "+second_name+" was "+third_adj+".")
+print("That is the story of "+first_name+" and "+second_name+".")
+
